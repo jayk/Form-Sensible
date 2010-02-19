da6b1d4b033c0470d66d16b142add9957d53f85b
Jay Kuri <jayk@ubuntu.(none)>
2010-02-19 15:11:14 -0700

    adding changes and Manifest adjustments

--- lib/Form/Sensible.pm
+++ lib/Form/Sensible.pm
@@ -16,7 +16,7 @@ use Form::Sensible::Validator;
 use Form::Sensible::Validator::Result;
 
 
-our $VERSION = "0.11100";
+our $VERSION = "0.11101";
 
 ## This module is a simple factory class which will load and create the various
 ## types of modules required when working with Form::Sensible
