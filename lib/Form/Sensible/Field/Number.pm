f3d3bd7ed6646e11abf9e1e08808978b60243224
David Romano <unobe@cpan.org>
2012-01-06 16:03:07 -0700

    [Field::Number] formatting and docs

--- lib/Form/Sensible/Field/Number.pm
+++ lib/Form/Sensible/Field/Number.pm
@@ -1,6 +1,6 @@
 package Form::Sensible::Field::Number;
 
-use Moose; 
+use Moose;
 use namespace::autoclean;
 extends 'Form::Sensible::Field';
 
@@ -33,7 +33,7 @@ has 'step' => (
 around 'validate' => sub {
     my $orig = shift;
     my $self = shift;
-    
+
     my @errors;
     push @errors, $self->$orig(@_);
     
@@ -43,13 +43,12 @@ around 'validate' => sub {
         if (ref($regex) ne 'REGEX') {            
             $regex = qr/$regex/;
         }
-    
     }
-    
+
     if ($self->value !~ $regex ) {
         push @errors, "_FIELDNAME_ is not a number";
     }
-    
+
     if (defined($self->lower_bound) && $self->value < $self->lower_bound) {
         push @errors, "_FIELDNAME_ is lower than the minimum allowed value";
     }
@@ -60,7 +59,7 @@ around 'validate' => sub {
     if ( $self->integer_only && $self->value != int($self->value)) {
         push @errors, "_FIELDNAME_ must be an integer.";
     }
-    
+
     ## we ran the gauntlet last check is to see if value is in step.
     if (defined($self->step) && !$self->in_step()) {
         push @errors, "_FIELDNAME_ must be a multiple of " . $self->step;
@@ -72,7 +71,7 @@ around 'validate' => sub {
 ## this is used when generating a slider or select of valid values.
 sub get_potential_values {
     my ($self, $step, $lower_bound, $upper_bound) = @_;
-    
+
     if (!$step) {
         $step = $self->step || 1;
     } 
@@ -84,11 +83,11 @@ sub get_potential_values {
     }
     
     my $value = $lower_bound;
-    
+
     ## this check ensures that we start with a value that is within our
-    ## bounds.  If $self->lower_bound does not lie on a step boundry, 
+    ## bounds.  If $self->lower_bound does not lie on a step boundary,
     ## and we generated all our numbers from lower_bound, we would be
-    ## producing a bunch of options that were always invalid. 
+    ## producing a bunch of options that were always invalid.
     ## Technically speaking, we shouldn't have a lower bound that is invalid
     ## but who are we kidding?  It will happen.
     
@@ -97,19 +96,19 @@ sub get_potential_values {
         # multiply by step.  Should be the first value that lies above our
         # provided bound.
         my $div = $value / $step;
-        
+
         $value = (int($div)+1) * $step;
     }
-    
+
     my @vals;
     while ($value <= $upper_bound) {
         push @vals, $value;
-        $value+= $step;
+        $value += $step;
     }
     return @vals;
 }
 
-## this allows a number to behave like a Select if used that way.
+## this allows a Number to behave like a Select if used that way.
 sub get_options {
     my ($self) = @_;
     
@@ -118,25 +117,25 @@ sub get_options {
 
 sub accepts_multiple {
     my ($self) = @_;
-    
+
     return 0;
 }
 
 sub in_step {
     my ($self, $value, $step) = @_;
-    
+
     if (!$step) {
         $step = $self->step;
     }
     if (!defined($value)) {
         $value = $self->value;
     }
-    
+
     ## we have to do the step check this way, because % will not deal with
     ## a fractional step value.
     my $div = $value / $step;
     return ($div == int($div));
-    
+
 }
 
 sub get_additional_configuration {
@@ -164,7 +163,7 @@ Form::Sensible::Field::Number - A Numeric field type.
 
     use Form::Sensible::Field::Number;
     
-    my $object = Form::Sensible::Field::Number->new( 
+    my $object = Form::Sensible::Field::Number->new(
                                                         integer_only => 1,
                                                         lower_bound => 10,
                                                         upper_bound => 100,
@@ -175,16 +174,16 @@ Form::Sensible::Field::Number - A Numeric field type.
 
 =head1 DESCRIPTION
 
-The number field type is one of the more advanced 
-field types in Form::Sensible. It has a number of features for
-dealing specifically with numbers.  It can be set to have a lower and
-upper bound, allowing validation to ensure that the value selected is
-within a range.  It can also be set to have a 'step', which provides a
-constraint to what values are valid between the upper and lower bounds.
-It can also be made to accept integers only, or fractional values.  
+The number field type is one of the more advanced field types in
+Form::Sensible. It has a number of features for dealing specifically with
+numbers.  It can be set to have a lower and upper bound, allowing validation
+to ensure that the value selected is within a range.  It can also be set to
+have a 'step', which provides a constraint to what values are valid between
+the upper and lower bounds.  It can also be made to accept integers only, or
+fractional values.
 
-Finally, it can be rendered in a number of ways including select boxes,
-drop downs or even ranged-sliders if your renderer supports it.
+Finally, it can be rendered in a number of ways including select boxes, drop
+downs or even ranged-sliders if your renderer supports it.
 
 =head1 ATTRIBUTES
 
@@ -193,7 +192,7 @@ drop downs or even ranged-sliders if your renderer supports it.
 =item C<integer_only>
 
 True/false value indicating whether this field is able to accept fractional
-values.
+values. Required attribute.
 
 =item C<lower_bound>
 
@@ -203,12 +202,12 @@ Lower bound of valid values for the field.
 
 Upper bound of valid values for the field.
 
-=item C<step> 
+=item C<step>
 
 When step is provided, the value provided must be a multiple of C<step> in
 order to be valid.
 
-=back 
+=back
 
 =head1 METHODS
 
@@ -220,22 +219,27 @@ Validates the field against the numeric constraints set for the field.
 
 =item C<get_potential_values($step, $lower_bound, $upper_bound)>
 
-Returns an array containing all the valid values between the 
+Returns an array containing all the valid values between the
 upper and lower bound.  Used internally to the number field.
 
 
-=item C<in_step($value, $step)> 
+=item C<in_step($value, $step)>
 
 Returns true if $value lies on a $step boundary. Otherwise returns false.
 
-=back 
+=item C<get_additional_configuration>
 
-The following two methods allow Number fields to be treated like Select 
+Returns a hashref consisting of the attributes for this field and their
+values.
+
+=back
+
+The following two methods allow Number fields to be treated like Select
 Fields for rendering purposes.
 
-=over 8 
+=over 8
 
-=item C<options>
+=item C<get_options>
 
 An array ref containing the allowed options. Each option is represented as a
 hash containing a C<name> element and a C<value> element for the given option.
@@ -243,7 +247,8 @@ hash containing a C<name> element and a C<value> element for the given option.
 =item C<accepts_multiple>
 
 On a Select field, this defines whether a the field can have multiple values.  For
-a Number field, only one value is allowed, so this always returns false. 
+a Number field, only one value is allowed, so this always returns false.
+
 
 =back
 
@@ -267,8 +272,8 @@ notation, you can provide a replacement regex in the field's 'validation' hash:
                                                 );
 
 Note that the Number validation routines outlined above are not built to
-handle numbers Perl can not handle natively. That is to say, if you are
-working with numbers that Perl can not parse, or that require you to use
+handle numbers perl can not handle natively. That is to say, if you are
+working with numbers that perl can not parse, or that require you to use
 modules such as L<Math::BigInt>, you can still use the Number class, but it's
 best to avoid Number's builtin validation. You can perform your validation
 yourself in a C<code> validation block or by subclassing Form::Sensible's
