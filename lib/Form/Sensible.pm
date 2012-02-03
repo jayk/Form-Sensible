bdc8f4412b313d44e0716bee5e87cb40a819e3ce
Jay Kuri <jayk@Jasons-MacBook-Air.local>
2012-02-02 17:00:55 -0700

    Update changes and version preparing for a push to cpan

--- lib/Form/Sensible.pm
+++ lib/Form/Sensible.pm
@@ -17,7 +17,7 @@ use Form::Sensible::Validator;
 use Form::Sensible::Validator::Result;
 use Form::Sensible::DelegateConnection;
 
-our $VERSION = "0.20012";
+our $VERSION = "0.20020";
 
 Moose::Exporter->setup_import_methods(
       also     => [ 'Form::Sensible::DelegateConnection' ]  
